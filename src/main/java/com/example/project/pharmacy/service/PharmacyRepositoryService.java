da15e1dd33e603a09856a54e58e92444476516a7
hongsh429 <shh0429@kakao.com>
2023-07-27 00:45:12 +0900

    spring AOP 기반으로 하는 기능들 (@Transactional @Cachesable @Async) + Self invocation 문제

--- src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.util.ObjectUtils;
 
+import java.util.List;
 import java.util.Objects;
 
 
@@ -18,6 +20,38 @@ import java.util.Objects;
 public class PharmacyRepositoryService {
 
     private final PharmacyRepository pharmacyRepository;
+
+
+
+    // self invocation test
+    @Transactional
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList); // self invocation test
+    }
+
+    // self invocation test
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error"); // 예외 발생 -> Roll Back 되어야 한다!
+        });
+    }
+
+
+    // read only test
+    @Transactional(readOnly = true) // 영속성 컨텍스트에 스넵샷을 저장하는 부분이 더이상 사용하지 않음. 그래서 조회용으로 사용되며 약간의 성능 향상을 볼 수 있다.
+    public void startReadOnlyMethod(Long id) {
+        pharmacyRepository.findById(id).ifPresent(pharmacy ->
+                pharmacy.changePharmacyAddress("서울 특별시 광진구"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
+    }
 
     @Transactional
     public void updateAddress(Long id, String address) {
@@ -40,4 +74,10 @@ public class PharmacyRepositoryService {
         }
         entity.changePharmacyAddress(address);
     }
+
+
+    /*
+    Class 단위의 Transactional 보다 메서드 단위의 Transactional의 우선 순위가 더 높다.
+
+    */
 }
--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -2,13 +2,19 @@ package com.example.project.pharmacy.repository
 
 import com.example.project.AbstractIntegrationContainerBaseTest
 import com.example.project.pharmacy.entity.Pharmacy
+import com.example.project.pharmacy.service.PharmacyRepositoryService
 import org.springframework.beans.factory.annotation.Autowired
+
+import java.time.LocalDateTime
 
 // 통합 테스트 환경
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
     private PharmacyRepository pharmacyRepository
+
+    @Autowired
+    private PharmacyRepositoryService pharmacyRepositoryService
 
     def setup() {
         pharmacyRepository.deleteAll()
@@ -58,6 +64,77 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
         then:
         result.size() == 1
+    }
+
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
+
+    }
+
+    def "self invocation"() {
+
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepositoryService.bar(Arrays.asList(pharmacy))
+        // 내부에서 foo라는 메서드를 호출하고 그 안에서 self-invocation 이 일어난다.(bar<내부 메서드> -> foo<내부 메서드>) 내부에서 내부를 호출할 때!
+
+        then:
+        def e = thrown(RuntimeException.class)
+        def result = pharmacyRepositoryService.findAll()
+        result.size() == 1 // 트랜잭션이 적용되지 않는다( 롤백 적용 X )  만약 롤백이 되었다면, size() == 0 이 나와야 한다.
+    }
+
+    def "transactional readOnly test - 읽기 전용일 경우 dirty checking 반영 되지 않는다. "() {
+
+        given:
+        String inputAddress = "서울 특별시 성북구"
+        String modifiedAddress = "서울 특별시 광진구"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def input = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def pharmacy = pharmacyRepository.save(input)
+        pharmacyRepositoryService.startReadOnlyMethod(pharmacy.id)
+
+        then:
+        def result = pharmacyRepositoryService.findAll()
+        result.get(0).getPharmacyAddress() == inputAddress
     }
 
 }
